8a424ff811c19027f30a93cb2dc00fdd3a54d39e
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2021-10-20 09:43:53 +0300

    Models

--- csv_app/admin.py
+++ csv_app/admin.py
@@ -1,3 +1,11 @@
+from django import forms
 from django.contrib import admin
 
+from .models import ColumnSet, Column, Type
+
 # Register your models here.
+
+
+admin.site.register(Column)
+admin.site.register( Type)
+admin.site.register(ColumnSet)
--- csv_app/models.py
+++ csv_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+# Data types:
+# Full name (a combination of first name and last name)
+# Job
+# Email
+# Domain name
+# Phone number
+# Company name
+# Text (with specified range for a number of sentences)
+# Integer (with specified range)
+# Address
+# Date
+
+# class SchemaColumn(models.Model):
+#
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    order = models.IntegerField()
+
+    def __str__(self):
+        return f'cul name {self.name} type {self.type} order {self.order}'
+
+
+class ColumnSet(models.Model):
+    name = models.CharField(max_length=50)
+    column = models.ManyToManyField(Column)
+    download_link = models.CharField(max_length=150, blank=True)
+    def __str__(self):
+        return f' {self.name}'
